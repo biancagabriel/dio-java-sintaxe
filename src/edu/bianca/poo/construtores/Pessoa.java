4681a6cc784ce4d8dacc01694931bbf4df09e2f0
Bianca Gabriel <bianca.gabriel14@gmail.com>
2024-05-10 14:37:15 -0300

    conceito de construtores

---
+++ src/edu/bianca/poo/construtores/Pessoa.java
@@ -0,0 +1,25 @@
+package edu.bianca.poo.construtores;
+
+public class Pessoa {
+    private String nome;
+    private String cpf;
+    private String endereco;
+
+    public Pessoa (String cpf, String nome) {
+        this.cpf = cpf;
+        this.nome = nome;
+    } //método construtor
+
+    public String getNome() {
+        return nome;
+    }
+    public String getCpf() {
+        return cpf;
+    }
+    public String getEndereco() {
+        return endereco;
+    }
+    public void setEndereco(String endereco) {
+        this.endereco = endereco;
+    }
+}
